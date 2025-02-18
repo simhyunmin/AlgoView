de765e30bb0a211f715297794ef67ff0f4e2e2a6
simhyunmin <shm66371945@gmail.com>
2025-02-18 18:41:33 +0900

    feat : 프로젝트 파일 생성

---
+++ AlgoView_Server/src/main/java/AlgoView/AlgoView_Server/AlgoViewServerApplication.java
@@ -0,0 +1,13 @@
+package AlgoView.AlgoView_Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoViewServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgoViewServerApplication.class, args);
+	}
+
+}
